765374aa5361696741aed27b8419b2fdd75ac8a3
Ayomide <ayomidelawal700@gmail.com>
2023-08-07 04:31:00 +0100

    setup neon postgress

--- config/db/intialize.ts
+++ config/db/intialize.ts
@@ -1,8 +1,9 @@
-import postgres from 'postgres';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import * as schema from '../../schema/';
-const client = postgres('');
+// import postgres from 'postgres';
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import * as schema from '@/schema/index';
+// import { parsedEnv } from '../env';
 
-const db = drizzle(client, { schema });
+// const client = postgres(parsedEnv.DATABASE_URL);
+// const db = drizzle(client, { schema });
 
-export { db };
+// export { db };
--- config/db/migrate.ts
+++ config/db/migrate.ts
@@ -1,13 +1,14 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import { migrate } from 'drizzle-orm/postgres-js/migrator';
+// import postgres from 'postgres';
+// import { parsedEnv } from '../env';
 
-const migrationClient = postgres('', { max: 1 });
-migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' })
-  .then(() => {
-    console.log('migration completed');
-  })
-  .catch((e) => {
-    console.log('something went wrong while migrating');
-    console.log(e);
-  });
+// const migrationClient = postgres(parsedEnv.DATABASE_URL, { max: 1 });
+// migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' })
+//   .then(() => {
+//     console.log('migration completed');
+//   })
+//   .catch((e) => {
+//     console.log('something went wrong while migrating');
+//     console.log(e);
+//   });
