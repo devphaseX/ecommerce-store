2e183ded03a48ebbb7c41cd9541e485ec1843075
Ayomide <ayomidelawal700@gmail.com>
2023-08-08 17:24:39 +0100

    render active store in dashboard

--- app/(dashboard)/[storeId]/(route)/page.tsx
+++ app/(dashboard)/[storeId]/(route)/page.tsx
@@ -1,3 +1,26 @@
-export const DashboardPage = () => {
-  return <div>This is a dashboard</div>;
+import { db } from '@/config/db/neon/initialize';
+import { stores } from '@/schema/store';
+import { auth } from '@clerk/nextjs';
+import { sql } from 'drizzle-orm';
+import { redirect } from 'next/navigation';
+import { FC } from 'react';
+
+interface DashboardPageProps extends PageBaseProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: FC<DashboardPageProps> = async ({ params }) => {
+  const { userId } = auth();
+  if (!userId) return redirect('/sign-in');
+
+  const { storeId } = params;
+
+  const store = await db.query.stores.findFirst({
+    where: sql`${stores.id} = ${storeId} and ${stores.userId} = ${userId}`,
+  });
+
+  if (!store) return redirect('/');
+  return <div>Active store name: {store.name}</div>;
 };
+
+export default DashboardPage;
