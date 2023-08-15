e2626d2916952550f27fdc7865872e7af1c47b0e
Ayomide <ayomidelawal700@gmail.com>
2023-08-15 09:43:20 +0100

    billboard table colum cell properties

--- app/(dashboard)/[storeId]/(route)/billboards/components/column.tsx
+++ app/(dashboard)/[storeId]/(route)/billboards/components/column.tsx
@@ -1,4 +1,5 @@
 import { ColumnDef } from '@tanstack/react-table';
+import { CellAction } from './cell-action';
 
 export type BillBoardColumns = {
   id: string;
@@ -9,4 +10,5 @@ export type BillBoardColumns = {
 export const billboardColumns: ColumnDef<BillBoardColumns>[] = [
   { accessorKey: 'label', header: 'Label' },
   { accessorKey: 'createdAt', header: 'Date' },
+  { id: 'actions', cell: ({ row }) => <CellAction data={row.original} /> },
 ];
