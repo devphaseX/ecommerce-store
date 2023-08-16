be5cd17f603124f0f4035b7e426661bad1d9b947
Ayomide <ayomidelawal700@gmail.com>
2023-08-16 04:31:46 +0100

    remove unnecesary imports

--- app/(dashboard)/[storeId]/(route)/categories/[categoryId]/(validators)/param-schema.ts
+++ app/(dashboard)/[storeId]/(route)/categories/[categoryId]/(validators)/param-schema.ts
@@ -2,7 +2,7 @@ import {
   categoryIdParamSchema,
   storeIdParamSchema,
 } from '@/app/api/(params)/params-schema';
-import { TypeOf, object } from 'zod';
+import { TypeOf } from 'zod';
 
 const singleCategoryParamSchema = storeIdParamSchema.and(categoryIdParamSchema);
 type SingleCategoryParam = TypeOf<typeof singleCategoryParamSchema>;
