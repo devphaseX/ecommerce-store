50023a2d80dae8645fc0bde084edd6d0722e4cd9
Ayomide <ayomidelawal700@gmail.com>
2023-08-16 04:29:53 +0100

    param parser for size dynamic path

--- app/api/(params)/params-schema.ts
+++ app/api/(params)/params-schema.ts
@@ -1,7 +1,21 @@
-import { AnyZodObject, TypeOf, ZodObject, object, string } from 'zod';
+import {
+  AnyZodObject,
+  TypeOf,
+  ZodObject,
+  nativeEnum,
+  object,
+  string,
+  z,
+} from 'zod';
 
-type LabelParamKey<Param> = {
+enum DynamicPath {
+  UUID = 'uuid',
+  PATH = 'path',
+}
+
+type LabelParamKey<Param, Defer extends boolean> = {
   readonly paramName: Param;
+  defer?: Defer;
 };
 
 type TypeLooseDynamicZodObject<
@@ -11,22 +25,6 @@ type TypeLooseDynamicZodObject<
   [P in K]: InferZodSchemaObject<Type>[string];
 }>;
 
-function createIdParamQuerySchema<K extends `${string}Id`>(
-  param: LabelParamKey<K>
-) {
-  const key = param.paramName;
-
-  const schema = object({
-    [param.paramName]: string({
-      required_error: getRequiredParamsMessage(key),
-    })
-      .uuid({ message: getNonUuidTypeMessage(key) })
-      .nonempty(),
-  });
-
-  return <TypeLooseDynamicZodObject<K, typeof schema>>schema;
-}
-
 type InferZodSchemaObject<ZodSchemaObject extends object> =
   ZodSchemaObject extends ZodObject<infer Schema> ? Schema : never;
 
@@ -34,12 +32,73 @@ type ParamWith<T> = Expand<T & { [key: string]: unknown }>;
 export const getRequiredParamsMessage = (key: string) =>
   `[Param] - ${key} is required`;
 
+type ParamDeferQuerySchemaFn<K extends string, Schema extends AnyZodObject> = (
+  validateAsUUID?: boolean
+) => TypeLooseDynamicZodObject<K, Schema>;
+
+type ParamQuerySchema<
+  K extends string,
+  Defer extends boolean,
+  Schema extends AnyZodObject
+> = Defer extends false | undefined
+  ? TypeLooseDynamicZodObject<K, Schema>
+  : ParamDeferQuerySchemaFn<K, Schema>;
+
+function createIdParamQuerySchema<
+  K extends `${string}Id`,
+  Defer extends boolean
+>(option: LabelParamKey<K, Defer>) {
+  const key = option.paramName;
+
+  function createSchema(treatAsUUID?: boolean) {
+    const stringSchema = string({
+      required_error: getRequiredParamsMessage(key),
+    });
+    const schema = object({
+      [option.paramName]: treatAsUUID
+        ? stringSchema.uuid({ message: getNonUuidTypeMessage(key) })
+        : stringSchema.nonempty(),
+    });
+    return schema;
+  }
+
+  type SchemaType = ReturnType<typeof createSchema>;
+
+  if (option.defer) {
+    return createSchema as ParamQuerySchema<K, Defer, SchemaType>;
+  }
+
+  return createSchema() as ParamQuerySchema<K, Defer, SchemaType>;
+}
+
+function createDynamicPathSchema<K extends string, Schema extends AnyZodObject>(
+  parser: ParamDeferQuerySchemaFn<K, Schema>
+) {
+  return z.union([
+    parser(true).extend({
+      type: nativeEnum(DynamicPath).default(DynamicPath.UUID),
+    }),
+    parser().extend({
+      type: nativeEnum(DynamicPath).default(DynamicPath.PATH),
+    }),
+  ]);
+}
+
 export const getNonUuidTypeMessage = (key: string) =>
   `Expect ${key} of type UUID`;
-const storeIdParamSchema = createIdParamQuerySchema({ paramName: 'storeId' });
+const storeIdParamSchema = createIdParamQuerySchema({
+  paramName: 'storeId',
+  defer: false,
+});
 
 const categoryIdParamSchema = createIdParamQuerySchema({
   paramName: 'categoryId',
+  defer: false,
+});
+
+const sizeIdParamSchema = createIdParamQuerySchema({
+  paramName: 'sizeId',
+  defer: true,
 });
 
 const billboardIdParamSchema = object({
@@ -53,6 +112,19 @@ const billboardIdParamSchema = object({
 type ParamWithStoreId = ParamWith<TypeOf<typeof storeIdParamSchema>>;
 type ParamWithBillboardId = ParamWith<TypeOf<typeof billboardIdParamSchema>>;
 type ParamWithCategoryId = ParamWith<TypeOf<typeof categoryIdParamSchema>>;
+type ParamWithSizeId = ParamWith<TypeOf<ReturnType<typeof sizeIdParamSchema>>>;
 
-export { storeIdParamSchema, billboardIdParamSchema, categoryIdParamSchema };
-export type { ParamWithStoreId, ParamWithBillboardId, ParamWithCategoryId };
+export {
+  storeIdParamSchema,
+  billboardIdParamSchema,
+  categoryIdParamSchema,
+  sizeIdParamSchema,
+  DynamicPath,
+  createDynamicPathSchema,
+};
+export type {
+  ParamWithStoreId,
+  ParamWithBillboardId,
+  ParamWithCategoryId,
+  ParamWithSizeId,
+};
