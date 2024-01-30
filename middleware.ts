52c705d6c79d1fa7cd794bff2fd9a5b772901579
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-01-30 19:48:41 +0100

    fix middlware match issue

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ import { authMiddleware } from '@clerk/nextjs';
 export default authMiddleware({ publicRoutes: ['/api/:path*'] });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
